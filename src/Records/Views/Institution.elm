2b4048a034313b854186808e726226f9bee991b8
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-03-16 16:14:29 +0100

    Make institution page formatting better

--- src/Records/Views/Institution.elm
+++ src/Records/Views/Institution.elm
@@ -1,11 +1,24 @@
 module Records.Views.Institution exposing (..)
 
 import Api.Records exposing (InstitutionBody)
-import Element exposing (Element, column, el, text)
-import Language exposing (Language, extractLabelFromLanguageMap)
+import Element exposing (Element, alignTop, column, fill, height, px, row, width)
+import Language exposing (Language)
 import Records.DataTypes exposing (Msg)
+import UI.Components exposing (h2)
 
 
 viewInstitutionRecord : InstitutionBody -> Language -> Element Msg
 viewInstitutionRecord body language =
-    column [] [ el [] (text (extractLabelFromLanguageMap language body.label)) ]
+    row
+        [ alignTop
+        , width fill
+        ]
+        [ column
+            [ width fill ]
+            [ row
+                [ width fill
+                , height (px 120)
+                ]
+                [ h2 language body.label ]
+            ]
+        ]
