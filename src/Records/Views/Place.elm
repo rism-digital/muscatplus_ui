bf08838a079414dd0d519a94e87939a07ccf8108
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-04-01 15:48:25 +0200

    New: Add very basic place template
    Shows the place name, but not much else.

--- src/Records/Views/Place.elm
+++ src/Records/Views/Place.elm
@@ -1,10 +1,23 @@
 module Records.Views.Place exposing (..)
 
-import Element exposing (Element, none)
+import Element exposing (Element, alignTop, column, fill, height, none, px, row, width)
 import Records.DataTypes exposing (Msg, PlaceBody)
 import Shared.Language exposing (Language)
+import UI.Components exposing (h2)
 
 
 viewPlaceRecord : PlaceBody -> Language -> Element Msg
 viewPlaceRecord body language =
-    none
+    row
+        [ width fill
+        , alignTop
+        ]
+        [ column
+            [ width fill ]
+            [ row
+                [ width fill
+                , height (px 120)
+                ]
+                [ h2 language body.label ]
+            ]
+        ]
