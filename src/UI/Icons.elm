9952399182998ae447d3dc476c7b7212200f5a04
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-04-06 16:37:00 +0200

    New: Use the heroicons
    As good as any

---
+++ src/UI/Icons.elm
@@ -0,0 +1,27 @@
+module UI.Icons exposing (..)
+
+import Element exposing (Element, html)
+import Heroicons.Outline as Icon
+import Html.Attributes exposing (style)
+
+
+modeIcons :
+    { everything : Element msg
+    , sources : Element msg
+    , people : Element msg
+    , institutions : Element msg
+    , incipits : Element msg
+    , unknown : Element msg
+    }
+modeIcons =
+    let
+        iconWidth =
+            style "width" "1rem"
+    in
+    { everything = html (Icon.globeAlt [ iconWidth ])
+    , sources = html (Icon.bookOpen [ iconWidth ])
+    , people = html (Icon.userGroup [ iconWidth ])
+    , institutions = html (Icon.library [ iconWidth ])
+    , incipits = html (Icon.musicNote [ iconWidth ])
+    , unknown = html (Icon.x [ iconWidth ])
+    }
