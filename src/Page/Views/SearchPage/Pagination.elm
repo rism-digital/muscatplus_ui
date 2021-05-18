6305f8897415360c91c513b920ad1458dffa9fa4
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-05-18 10:43:54 +0200

    New: Make pagination better

--- src/Page/Views/SearchPage/Pagination.elm
+++ src/Page/Views/SearchPage/Pagination.elm
@@ -1,9 +1,11 @@
 module Page.Views.SearchPage.Pagination exposing (..)
 
-import Element exposing (Element, alignBottom, el, fill, link, none, row, text, width)
+import Element exposing (Element, alignBottom, alignLeft, alignRight, centerX, centerY, column, el, fill, height, link, none, padding, px, row, shrink, text, width)
+import Element.Font as Font
 import Language exposing (Language, extractLabelFromLanguageMap, localTranslations)
 import Msg exposing (Msg)
 import Page.RecordTypes.Search exposing (SearchPagination)
+import Page.UI.Images exposing (chevronDoubleLeftSvg, chevronDoubleRightSvg, chevronLeftSvg, chevronRightSvg)
 
 
 viewSearchResultsPagination : SearchPagination -> Language -> Element Msg
@@ -12,11 +14,42 @@ viewSearchResultsPagination pagination language =
         [ width fill
         , alignBottom
         ]
-        [ viewPaginationFirstLink (Just pagination.first) language
-        , viewPaginationPreviousLink pagination.previous language
-        , pageInfo pagination language
-        , viewPaginationNextLink pagination.next language
-        , viewPaginationLastLink pagination.last language
+        [ column
+            [ alignLeft
+            , width fill
+            ]
+            [ row
+                [ width shrink
+                , alignLeft
+                ]
+                [ viewPaginationFirstLink (Just pagination.first) language
+                , viewPaginationPreviousLink pagination.previous language
+                ]
+            ]
+        , column
+            [ centerX
+            , width fill
+            ]
+            [ row
+                [ width shrink
+                , height shrink
+                , centerX
+                , centerY
+                ]
+                [ pageInfo pagination language ]
+            ]
+        , column
+            [ alignRight
+            , width fill
+            ]
+            [ row
+                [ width shrink
+                , alignRight
+                ]
+                [ viewPaginationNextLink pagination.next language
+                , viewPaginationLastLink pagination.last language
+                ]
+            ]
         ]
 
 
@@ -33,14 +66,35 @@ pageInfo pagination language =
             extractLabelFromLanguageMap language localTranslations.page
     in
     el
-        []
+        [ Font.medium ]
         (text (label ++ " " ++ thisPage ++ " / " ++ totalPages))
 
 
-viewPaginationLink : Maybe String -> String -> Element Msg
-viewPaginationLink url linkText =
-    Maybe.map (\l -> el [] (link [] { url = l, label = text linkText })) url
+{-|
+
+    Maybe.map handles the case where the URL is `Nothing` and returns
+    the empty element `none`. So if there is a URL, this will return a
+    formatted pagination element; if there isn't, it will return an
+    empty element.
+
+-}
+viewPaginationLink : Element Msg -> Maybe String -> String -> Element Msg
+viewPaginationLink icon url linkText =
+    Maybe.map (\l -> formatPaginationLink icon l linkText) url
         |> Maybe.withDefault none
+
+
+formatPaginationLink : Element Msg -> String -> String -> Element Msg
+formatPaginationLink icon url linkText =
+    el
+        [ padding 5
+        ]
+        (link
+            [ height (px 20)
+            , width (px 20)
+            ]
+            { url = url, label = icon }
+        )
 
 
 viewPaginationNextLink : Maybe String -> Language -> Element Msg
@@ -49,7 +103,7 @@ viewPaginationNextLink url language =
         label =
             extractLabelFromLanguageMap language localTranslations.next
     in
-    viewPaginationLink url label
+    viewPaginationLink chevronRightSvg url label
 
 
 viewPaginationLastLink : Maybe String -> Language -> Element Msg
@@ -58,7 +112,7 @@ viewPaginationLastLink url language =
         label =
             extractLabelFromLanguageMap language localTranslations.last
     in
-    viewPaginationLink url label
+    viewPaginationLink chevronDoubleRightSvg url label
 
 
 viewPaginationPreviousLink : Maybe String -> Language -> Element Msg
@@ -67,7 +121,7 @@ viewPaginationPreviousLink url language =
         label =
             extractLabelFromLanguageMap language localTranslations.previous
     in
-    viewPaginationLink url label
+    viewPaginationLink chevronLeftSvg url label
 
 
 viewPaginationFirstLink : Maybe String -> Language -> Element Msg
@@ -76,4 +130,4 @@ viewPaginationFirstLink url language =
         label =
             extractLabelFromLanguageMap language localTranslations.first
     in
-    viewPaginationLink url label
+    viewPaginationLink chevronDoubleLeftSvg url label
