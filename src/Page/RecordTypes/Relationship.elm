fbf322d81c196b1e2bb24280ef1b02cdbb8c1832
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-06-01 11:56:39 +0200

    New: Improved relationship decoders
    Now types the relationships so that we can tell exactly what sort we're dealing with, and can adjust the behaviours of the views accordingly.

--- src/Page/RecordTypes/Relationship.elm
+++ src/Page/RecordTypes/Relationship.elm
@@ -1,5 +1,6 @@
 module Page.RecordTypes.Relationship exposing (..)
 
+import Dict
 import Json.Decode as Decode exposing (Decoder, andThen, list, string)
 import Json.Decode.Pipeline exposing (optional, required)
 import Language exposing (LanguageMap)
@@ -13,11 +14,151 @@ type RelatedTo
     | UnknownRelationship
 
 
+type RelationshipQualifier
+    = AscertainedQualifier
+    | VerifiedQualifier
+    | ConjecturalQualifier
+    | AllegedQualifier
+    | DoubtfulQualifier
+    | MisattributedQualifier
+    | UnknownQualifier
+
+
+{-|
+
+    Maps the expected value in the 'qualifier' section
+    to a type-defined value.
+
+-}
+qualifierMap : List ( String, RelationshipQualifier )
+qualifierMap =
+    [ ( "rism:Ascertained", AscertainedQualifier )
+    , ( "rism:Verified", VerifiedQualifier )
+    , ( "rism:Conjectural", ConjecturalQualifier )
+    , ( "rism:Alleged", AllegedQualifier )
+    , ( "rism:Doubtful", DoubtfulQualifier )
+    , ( "rism:Misattributed", MisattributedQualifier )
+    ]
+
+
+qualifierConverter : String -> Decoder RelationshipQualifier
+qualifierConverter qualString =
+    -- if we can't find the value in the dictionary,
+    -- then assume it's unknown.
+    Dict.fromList qualifierMap
+        |> Dict.get qualString
+        |> Maybe.withDefault UnknownQualifier
+        |> Decode.succeed
+
+
+type RelationshipRole
+    = ArrangerRole
+    | AssigneeRole
+    | AssociatedNameRole
+    | AuthorRole
+    | BooksellerRole
+    | ConceptorRole
+    | ComposerRole
+    | CensorRole
+    | CopyrightHolderRole
+    | ComposerAuthorRole
+    | ContributorRole
+    | DepositorRole
+    | DistributorRole
+    | DedicateeRole
+    | EditorRole
+    | EngraverRole
+    | EventPlaceRole
+    | FormerOwnerRole
+    | IllustratorRole
+    | LibrettistRole
+    | LicenseeRole
+    | LithographerRole
+    | LyricistRole
+    | OtherRole
+    | PublisherRole
+    | PapermakerRole
+    | PerformerRole
+    | PrinterRole
+    | CopyistRole
+    | TranslatorRole
+    | TypeDesignerRole
+    | BrotherOfRole
+    | ChildOfRole
+    | ConfusedWithRole
+    | FatherOfRole
+    | MarriedToRole
+    | MotherOfRole
+    | RelatedToRole
+    | SisterOfRole
+    | UnknownRole
+
+
+{-|
+
+    Maps the expected values of the 'role' in the JSON response to
+    a type-defined value.
+
+-}
+roleMap : List ( String, RelationshipRole )
+roleMap =
+    [ ( "rism:arr", ArrangerRole )
+    , ( "rism:asg", AssigneeRole )
+    , ( "rism:asn", AssociatedNameRole )
+    , ( "rism:aut", AuthorRole )
+    , ( "rism:bsl", BooksellerRole )
+    , ( "rism:ccp", ConceptorRole )
+    , ( "rism:cmp", ComposerRole )
+    , ( "rism:cns", CensorRole )
+    , ( "rism:cph", CopyrightHolderRole )
+    , ( "rism:cre", ComposerAuthorRole )
+    , ( "rism:ctb", ContributorRole )
+    , ( "rism:dpt", DepositorRole )
+    , ( "rism:dst", DistributorRole )
+    , ( "rism:dte", DedicateeRole )
+    , ( "rism:edt", EditorRole )
+    , ( "rism:egr", EngraverRole )
+    , ( "rism:evp", EventPlaceRole )
+    , ( "rism:fmo", FormerOwnerRole )
+    , ( "rism:ill", IllustratorRole )
+    , ( "rism:lbt", LibrettistRole )
+    , ( "rism:lse", LicenseeRole )
+    , ( "rism:ltg", LithographerRole )
+    , ( "rism:lyr", LyricistRole )
+    , ( "rism:oth", OtherRole )
+    , ( "rism:pbl", PublisherRole )
+    , ( "rism:ppm", PapermakerRole )
+    , ( "rism:prf", PerformerRole )
+    , ( "rism:prt", PrinterRole )
+    , ( "rism:scr", CopyistRole )
+    , ( "rism:trl", TranslatorRole )
+    , ( "rism:tyd", TypeDesignerRole )
+    , ( "rism:brother_of", BrotherOfRole )
+    , ( "rism:child_of", ChildOfRole )
+    , ( "rism:confused_with", ConfusedWithRole )
+    , ( "rism:father_of", FatherOfRole )
+    , ( "rism:married_to", MarriedToRole )
+    , ( "rism:mother_of", MotherOfRole )
+    , ( "rism:other", OtherRole )
+    , ( "rism:related_to", RelatedToRole )
+    , ( "rism:sister_of", SisterOfRole )
+    ]
+
+
+roleConverter : String -> Decoder RelationshipRole
+roleConverter roleString =
+    Dict.fromList roleMap
+        |> Dict.get roleString
+        |> Maybe.withDefault UnknownRole
+        |> Decode.succeed
+
+
 type alias RelationshipBody =
     { summary : Maybe (List LabelValue)
-    , role : Maybe String
-    , qualifier : Maybe String
+    , role : RelationshipRole
+    , qualifier : RelationshipQualifier
     , relatedTo : Maybe RelatedToBody
+    , name : Maybe LanguageMap
     }
 
 
@@ -34,16 +175,24 @@ relatedToTypeDecoder =
         |> andThen relatedToConverter
 
 
+roleDecoder : Decoder RelationshipRole
+roleDecoder =
+    Decode.string
+        |> andThen roleConverter
+
+
+qualifierDecoder : Decoder RelationshipQualifier
+qualifierDecoder =
+    Decode.string
+        |> andThen qualifierConverter
+
+
 relatedToBodyDecoder : Decoder RelatedToBody
 relatedToBodyDecoder =
     Decode.succeed RelatedToBody
         |> required "id" string
         |> required "label" languageMapLabelDecoder
         |> required "type" relatedToTypeDecoder
-
-
-
---|> required "type" relatedToTypeDecoder
 
 
 relatedToConverter : String -> Decoder RelatedTo
@@ -66,6 +215,7 @@ relationshipBodyDecoder : Decoder RelationshipBody
 relationshipBodyDecoder =
     Decode.succeed RelationshipBody
         |> optional "summary" (Decode.maybe (list labelValueDecoder)) Nothing
-        |> optional "role" (Decode.maybe string) Nothing
-        |> optional "qualifier" (Decode.maybe string) Nothing
+        |> required "role" roleDecoder
+        |> required "qualifier" qualifierDecoder
         |> optional "relatedTo" (Decode.maybe relatedToBodyDecoder) Nothing
+        |> optional "name" (Decode.maybe languageMapLabelDecoder) Nothing
