c2496129a9e97b48fe7801ca2a61cf03f2c339b9
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-06-03 13:12:59 +0200

    New: View for a source page.

--- src/Page/Views/SourcePage.elm
+++ src/Page/Views/SourcePage.elm
@@ -1,14 +1,39 @@
 module Page.Views.SourcePage exposing (..)
 
-import Element exposing (Element, fill, height, none, row, width)
+import Element exposing (Element, column, fill, height, none, padding, row, width)
+import Element.Background as Background
 import Model exposing (Model)
 import Msg exposing (Msg)
+import Page.Model exposing (Response(..))
+import Page.Response exposing (ServerData(..))
+import Page.UI.Style exposing (colourScheme)
+import Page.Views.SourcePage.FullRecordPage exposing (viewFullSourcePage)
 
 
 view : Model -> Element Msg
 view model =
+    let
+        page =
+            model.page
+
+        response =
+            page.response
+
+        pageView =
+            case response of
+                Response (SourceData body) ->
+                    viewFullSourcePage body model.language
+
+                _ ->
+                    none
+    in
     row
         [ width fill
-        , height fill
         ]
-        []
+        [ column
+            [ width fill
+            , padding 20
+            , Background.color colourScheme.white
+            ]
+            [ pageView ]
+        ]
