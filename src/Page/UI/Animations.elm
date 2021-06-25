4b55cfe2d661753c23ba44827d0a4afbdb5f2bd3
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-06-25 15:28:02 +0200

    wip: animations

--- src/Page/UI/Animations.elm
+++ src/Page/UI/Animations.elm
@@ -1,10 +1,13 @@
-module Page.UI.Animations exposing (loadingBox)
+module Page.UI.Animations exposing (animatedColumn, animatedEl, animatedRow, loadingBox)
 
 import Css exposing (absolute, animationDuration, animationName, backgroundColor, backgroundImage, before, block, display, height, hidden, left, linearGradient2, overflow, pct, position, property, px, relative, rgb, rgba, sec, stop2, toRight, top, width)
-import Css.Animations as Animations exposing (keyframes)
+import Css.Animations exposing (keyframes)
 import Element exposing (Element)
 import Html.Styled exposing (Attribute, div, toUnstyled)
 import Html.Styled.Attributes exposing (css)
+import Simple.Animation as Animation exposing (Animation)
+import Simple.Animation.Animated as Animated
+import Simple.Animation.Property as P
 
 
 {-|
@@ -26,11 +29,7 @@ loadingStyles whooshWidth whooshHeight =
     css
         [ height (px whooshHeight)
         , width (px whooshWidth)
-
-        --, boxShadow5 (px 0) (px 2) (px 4) (px 0) (rgba 33 33 33 0.15)
         , backgroundColor (rgb 250 250 250)
-
-        --, borderRadius (px 4)
         , position relative
         , overflow hidden
         , before
@@ -44,8 +43,8 @@ loadingStyles whooshWidth whooshHeight =
             , backgroundImage (linearGradient2 toRight (stop2 (rgba 255 255 255 0) (pct 0)) (stop2 (rgba 241 244 249 1) (pct 50)) [ stop2 (rgba 255 255 255 0) (pct 100) ])
             , animationName
                 (keyframes
-                    [ ( 0, [ Animations.property "left" "-150px" ] )
-                    , ( 100, [ Animations.property "left" "100%" ] )
+                    [ ( 0, [ Css.Animations.property "left" "-150px" ] )
+                    , ( 100, [ Css.Animations.property "left" "100%" ] )
                     ]
                 )
             , animationDuration (sec 1.0)
@@ -53,3 +52,26 @@ loadingStyles whooshWidth whooshHeight =
             , property "animation-timing-function" "cubic-bezier(0.4, 0.0, 0.2, 1)"
             ]
         ]
+
+
+animatedEl : Animation -> List (Element.Attribute msg) -> Element msg -> Element msg
+animatedEl =
+    animatedUi Element.el
+
+
+animatedRow : Animation -> List (Element.Attribute msg) -> List (Element msg) -> Element msg
+animatedRow =
+    animatedUi Element.row
+
+
+animatedColumn : Animation -> List (Element.Attribute msg) -> List (Element msg) -> Element msg
+animatedColumn =
+    animatedUi Element.column
+
+
+animatedUi =
+    Animated.ui
+        { behindContent = Element.behindContent
+        , htmlAttribute = Element.htmlAttribute
+        , html = Element.html
+        }
