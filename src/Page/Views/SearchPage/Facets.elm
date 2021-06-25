cc96bd36a57d8eea1fc517b3de315f1d8570080a
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-06-25 15:23:22 +0200

    fixed: additional facet views

--- src/Page/Views/SearchPage/Facets.elm
+++ src/Page/Views/SearchPage/Facets.elm
@@ -1,20 +1,24 @@
 module Page.Views.SearchPage.Facets exposing (..)
 
-import Element exposing (Element, alignLeft, centerX, centerY, el, fill, height, none, paddingXY, px, row, spacing, text, width)
+import Dict
+import Element exposing (Element, alignLeft, centerX, centerY, column, el, fill, height, none, paddingXY, px, row, spacing, text, width)
 import Element.Border as Border
 import Element.Font as Font
 import Element.Input exposing (checkbox, labelLeft)
 import Language exposing (Language, extractLabelFromLanguageMap, formatNumberByLanguage)
 import Msg exposing (Msg(..))
+import Page.Query exposing (Filter(..))
 import Page.RecordTypes.ResultMode exposing (ResultMode, parseStringToResultMode)
-import Page.RecordTypes.Search exposing (Facet, FacetItem(..))
+import Page.RecordTypes.Search exposing (FacetData(..), FacetItem(..), FilterFacet, ModeFacet, RangeFacet, SearchBody, SelectorFacet, ToggleFacet)
 import Page.UI.Attributes exposing (bodyRegular)
+import Page.UI.Facets.Toggle as Toggle
 import Page.UI.Images exposing (institutionSvg, liturgicalFestivalSvg, musicNotationSvg, peopleSvg, sourcesSvg, unknownSvg)
 import Page.UI.Style exposing (colourScheme, convertColorToElementColor)
+import Search exposing (ActiveSearch)
 
 
-viewModeItems : ResultMode -> Facet -> Language -> Element Msg
-viewModeItems selectedMode typeFacet language =
+viewModeItems : ResultMode -> Language -> ModeFacet -> Element Msg
+viewModeItems selectedMode language typeFacet =
     let
         rowLabel =
             row
@@ -32,11 +36,11 @@ viewModeItems selectedMode typeFacet language =
         , spacing 10
         , centerY
         ]
-        (rowLabel :: List.map (\t -> viewModeItem selectedMode t language) typeFacet.items)
+        (rowLabel :: List.map (\t -> viewModeItem selectedMode language t) typeFacet.items)
 
 
-viewModeItem : ResultMode -> FacetItem -> Language -> Element Msg
-viewModeItem selectedMode fitem language =
+viewModeItem : ResultMode -> Language -> FacetItem -> Element Msg
+viewModeItem selectedMode language fitem =
     let
         -- uses opaque type destructuring to unpack the values of the facet item.
         (FacetItem value label count) =
@@ -115,3 +119,74 @@ viewModeItem selectedMode fitem language =
                 }
             )
         ]
+
+
+viewFacet : String -> Language -> ActiveSearch -> SearchBody -> Element Msg
+viewFacet facetKey language activeSearch body =
+    let
+        facetDict =
+            body.facets
+
+        facetConf =
+            Dict.get facetKey facetDict
+
+        query =
+            activeSearch.query
+
+        activeFilters =
+            query.filters
+
+        facetView =
+            case facetConf of
+                Just (ToggleFacetData facet) ->
+                    viewToggleFacet language activeFilters facet
+
+                Just (RangeFacetData facet) ->
+                    viewRangeFacet language activeFilters facet
+
+                _ ->
+                    none
+    in
+    facetView
+
+
+viewToggleFacet : Language -> List Filter -> ToggleFacet -> Element Msg
+viewToggleFacet language activeFilters facet =
+    let
+        facetAlias =
+            facet.alias
+
+        isActive =
+            List.any (\(Filter alias _) -> alias == facetAlias) activeFilters
+    in
+    row
+        []
+        [ column
+            []
+            [ row
+                []
+                [ el []
+                    (Toggle.view isActive (UserClickedFacetToggle facet.alias)
+                        |> Toggle.setLabel (extractLabelFromLanguageMap language facet.label)
+                        |> Toggle.render
+                    )
+                ]
+            ]
+        ]
+
+
+viewRangeFacet : Language -> List Filter -> RangeFacet -> Element Msg
+viewRangeFacet language activeFilters body =
+    row
+        []
+        [ el [] (text "Range facet!") ]
+
+
+viewSelectorFacet : Language -> List Filter -> SelectorFacet -> Element Msg
+viewSelectorFacet language activeFilters body =
+    none
+
+
+viewFilterFacet : Language -> List Filter -> FilterFacet -> Element Msg
+viewFilterFacet language activeFilters body =
+    none
