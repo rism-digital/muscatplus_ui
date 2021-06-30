24365daa51b84804a085970d0e812b72aaaa51d3
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-06-30 12:25:23 +0200

    new: subscription for range facets

--- src/Subscriptions.elm
+++ src/Subscriptions.elm
@@ -2,13 +2,31 @@ module Subscriptions exposing (subscriptions)
 
 import Browser.Events exposing (onResize)
 import Device exposing (detectDevice)
+import Dict
 import Model exposing (Model)
 import Msg exposing (Msg)
+import Page.UI.Facets.RangeSlider as RangeSlider
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
+    let
+        activeSearch =
+            model.activeSearch
+
+        sliders =
+            activeSearch.sliders
+
+        sliderSubscriptions =
+            Dict.toList sliders
+                |> List.map
+                    (\( k, v ) ->
+                        RangeSlider.subscriptions v
+                            |> Sub.map (Msg.UserMovedRangeSlider k)
+                    )
+    in
     Sub.batch
         [ onResize <|
             \width height -> Msg.UserResizedWindow (detectDevice width height)
+        , Sub.batch sliderSubscriptions
         ]
