e0d1359029794baf5896a49eb88a54e6d91bb836
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-08-23 16:07:16 +0200

    changes to optimize script
    this is based on some testing done by the elm community members

--- optimize.sh
+++ optimize.sh
@@ -6,7 +6,7 @@ js=$2
 min=$3
 
 elm make --optimize --output=$js $1
-uglifyjs $js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,passes=2,unsafe_comps,unsafe" | uglifyjs --mangle --output $min
+uglifyjs $js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,unsafe_comps,unsafe' --mangle 'reserved=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9]' --output $min
 
 echo "Initial size: $(cat $js | wc -c) bytes  ($js)"
 echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
