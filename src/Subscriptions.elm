eed85b3b78cc8ac80029004420e2e9ebd2227199
Andrew Hankinson <andrew.hankinson@gmail.com>
2021-12-21 18:36:17 +0100

    Forgotten range slider code

--- src/Subscriptions.elm
+++ src/Subscriptions.elm
@@ -2,10 +2,8 @@ module Subscriptions exposing (subscriptions)
 
 import Browser.Events exposing (onResize)
 import Device exposing (detectDevice)
-import Dict
 import Model exposing (Model)
 import Msg exposing (Msg)
-import Page.UI.Facets.RangeSlider as RangeSlider
 
 
 subscriptions : Model -> Sub Msg
