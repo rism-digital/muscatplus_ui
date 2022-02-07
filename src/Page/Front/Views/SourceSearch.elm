7b085fdc4bf7227f20eebdc91972298265d2de75
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-02-07 11:59:13 +0100

    Clean up imports

--- src/Page/Front/Views/SourceSearch.elm
+++ src/Page/Front/Views/SourceSearch.elm
@@ -1,22 +1,16 @@
 module Page.Front.Views.SourceSearch exposing (..)
 
-import Element exposing (Element, alignLeft, alignTop, column, fill, none, paragraph, row, spacing, text, width)
+import Element exposing (Element, alignTop, column, fill, paragraph, row, spacing, text, width)
 import Element.Font as Font
-import Element.Region as Region
-import Language exposing (Language, extractLabelFromLanguageMap, formatNumberByLanguage)
-import Language.LocalTranslations exposing (localTranslations)
 import Page.Front.Model exposing (FrontPageModel)
 import Page.Front.Msg as FrontMsg exposing (FrontMsg(..))
 import Page.Front.Views.Facets exposing (viewFrontFacet)
 import Page.Front.Views.SearchControls exposing (viewFrontKeywordQueryInput, viewFrontSearchButtons)
 import Page.Query exposing (toKeywordQuery, toNextQuery)
 import Page.RecordTypes.Front exposing (FrontBody)
-import Page.RecordTypes.Shared exposing (FacetAlias)
 import Page.UI.Attributes exposing (headingHero, lineSpacing, sectionSpacing)
 import Page.UI.Components exposing (dividerWithText)
-import Response exposing (Response(..), ServerData(..))
 import Session exposing (Session)
-import Utlities exposing (namedValue)
 
 
 sourceSearchPanelView : Session -> FrontPageModel -> FrontBody -> Element FrontMsg
