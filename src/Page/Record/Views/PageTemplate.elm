64c59468461db89334887f4f24c8c7962b9fd205
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-03-24 15:33:59 +0100

    Re-enable Muscat links

--- src/Page/Record/Views/PageTemplate.elm
+++ src/Page/Record/Views/PageTemplate.elm
@@ -1,13 +1,37 @@
 module Page.Record.Views.PageTemplate exposing (..)
 
-import Element exposing (Element, alignBottom, alignRight, column, el, fill, htmlAttribute, link, row, text, width)
+import Config as C
+import Element
+    exposing
+        ( Element
+        , alignBottom
+        , alignLeft
+        , alignRight
+        , column
+        , el
+        , fill
+        , htmlAttribute
+        , link
+        , none
+        , padding
+        , paddingXY
+        , row
+        , spacing
+        , text
+        , width
+        )
+import Element.Background as Background
+import Element.Border as Border
 import Html.Attributes as HTA
 import Language exposing (Language, LanguageMap, extractLabelFromLanguageMap)
 import Language.LocalTranslations exposing (localTranslations)
 import Page.Record.Views.RecordHistory exposing (viewRecordHistory)
 import Page.RecordTypes.Shared exposing (RecordHistory)
-import Page.UI.Attributes exposing (headingLG, linkColour)
+import Page.Route exposing (Route(..))
+import Page.UI.Attributes exposing (headingLG, lineSpacing, linkColour)
 import Page.UI.Components exposing (h1)
+import Page.UI.Style exposing (colourScheme, convertColorToElementColor)
+import Session exposing (Session)
 
 
 pageHeaderTemplate : Language -> { a | sectionToc : String, label : LanguageMap } -> Element msg
@@ -35,16 +59,79 @@ pageUriTemplate language body =
         ]
 
 
-pageFooterTemplate : Language -> { a | recordHistory : RecordHistory } -> Element msg
-pageFooterTemplate language footer =
+pageFooterTemplate : Session -> Language -> { a | recordHistory : RecordHistory } -> Element msg
+pageFooterTemplate session language footer =
+    let
+        muscatLinks =
+            if session.showMuscatLinks == True then
+                column
+                    [ width fill
+                    , alignLeft
+                    ]
+                    [ row
+                        [ width fill
+                        , alignLeft
+                        ]
+                        [ viewMuscatLinks session ]
+                    ]
+
+            else
+                none
+    in
     row
         [ width fill
+        , padding 20
         , alignBottom
+        , Border.widthEach { top = 2, bottom = 0, left = 0, right = 0 }
+        , Border.color (colourScheme.slateGrey |> convertColorToElementColor)
+        , Background.color (colourScheme.cream |> convertColorToElementColor)
         ]
-        [ column
+        [ muscatLinks
+        , column
             [ width fill
             , alignRight
             ]
             [ viewRecordHistory language footer.recordHistory
             ]
         ]
+
+
+viewMuscatLinks : Session -> Element msg
+viewMuscatLinks session =
+    let
+        linkTmpl muscatUrl =
+            column
+                [ alignLeft
+                , width fill
+                , paddingXY 5 0
+                , spacing lineSpacing
+                ]
+                [ row
+                    [ width fill ]
+                    [ text "Muscat: "
+                    , link
+                        [ linkColour ]
+                        { url = muscatUrl
+                        , label = text "View"
+                        }
+                    , text " | "
+                    , link
+                        [ linkColour ]
+                        { url = muscatUrl ++ "/edit"
+                        , label = text "Edit"
+                        }
+                    ]
+                ]
+    in
+    case session.route of
+        SourcePageRoute id ->
+            linkTmpl <| C.muscatLinkBase ++ "sources/" ++ String.fromInt id
+
+        PersonPageRoute id ->
+            linkTmpl <| C.muscatLinkBase ++ "people/" ++ String.fromInt id
+
+        InstitutionPageRoute id ->
+            linkTmpl <| C.muscatLinkBase ++ "institutions/" ++ String.fromInt id
+
+        _ ->
+            none
