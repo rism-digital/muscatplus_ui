591e8080d9601d90b0e8e488ec02d65aa5e225a6
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-04-12 09:34:22 +0200

    New: Add icon to source item links

--- src/Page/UI/Record/SourceItemsSection.elm
+++ src/Page/UI/Record/SourceItemsSection.elm
@@ -1,12 +1,14 @@
 module Page.UI.Record.SourceItemsSection exposing (..)
 
-import Element exposing (Element, alignTop, column, fill, height, link, row, spacing, text, width)
+import Element exposing (Element, alignTop, centerY, column, el, fill, height, link, px, row, spacing, text, width)
 import Language exposing (Language, extractLabelFromLanguageMap)
 import Page.RecordTypes.Source exposing (SourceItemsSectionBody)
 import Page.RecordTypes.SourceBasic exposing (BasicSourceBody)
 import Page.UI.Attributes exposing (headingMD, lineSpacing, linkColour, sectionBorderStyles)
 import Page.UI.Components exposing (viewSummaryField)
+import Page.UI.Images exposing (sourcesSvg)
 import Page.UI.Record.SectionTemplate exposing (sectionTemplate)
+import Page.UI.Style exposing (colourScheme)
 
 
 viewSourceItemsSection : Language -> SourceItemsSectionBody -> Element msg
@@ -26,8 +28,16 @@ viewSourceItem language source =
             , alignTop
             ]
             [ row
-                [ width fill ]
-                [ link
+                [ width fill
+                , spacing 5
+                ]
+                [ el
+                    [ width <| px 20
+                    , height <| px 20
+                    , centerY
+                    ]
+                    (sourcesSvg colourScheme.slateGrey)
+                , link
                     [ linkColour
                     , headingMD
                     ]
