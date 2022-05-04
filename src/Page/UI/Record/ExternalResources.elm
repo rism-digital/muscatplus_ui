0da428a2f346de7076e7fea97ddc006ce0bb73f1
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-05-04 09:18:58 +0200

    Add IIIF Viewer capability
    Adds the Universal Viewer for viewing IIIF manifests
    
    Refs #177

--- src/Page/UI/Record/ExternalResources.elm
+++ src/Page/UI/Record/ExternalResources.elm
@@ -1,10 +1,12 @@
 module Page.UI.Record.ExternalResources exposing (..)
 
-import Element exposing (Element, alignTop, column, fill, height, link, row, spacing, text, width, wrappedRow)
+import Config as C
+import Element exposing (Element, alignLeft, alignTop, column, el, fill, height, link, px, row, spacing, text, width, wrappedRow)
 import Language exposing (Language)
-import Page.RecordTypes.ExternalResource exposing (ExternalResourceBody, ExternalResourcesSectionBody)
+import Page.RecordTypes.ExternalResource exposing (ExternalResourceBody, ExternalResourceType(..), ExternalResourcesSectionBody)
 import Page.UI.Attributes exposing (lineSpacing, linkColour, sectionBorderStyles, sectionSpacing)
 import Page.UI.Components exposing (renderParagraph)
+import Page.UI.Images exposing (iiifLogo)
 import Page.UI.Record.SectionTemplate exposing (sectionTemplate)
 
 
@@ -34,6 +36,52 @@ viewExternalResourcesSection language extSection =
 
 viewExternalResource : Language -> ExternalResourceBody -> Element msg
 viewExternalResource language body =
+    let
+        externalResourceLink =
+            case body.type_ of
+                IIIFManifestResourceType ->
+                    [ column
+                        [ width fill
+                        , spacing 5
+                        ]
+                        [ row
+                            [ width fill
+                            , alignLeft
+                            , spacing 5
+                            ]
+                            [ link
+                                [ linkColour
+                                , alignLeft
+                                ]
+                                { url = C.serverUrl ++ "/viewer.html#?manifest=" ++ body.url
+                                , label = text "View Images"
+                                }
+                            , text "|"
+                            , el
+                                [ width (px 25)
+                                , height (px 26)
+                                , alignLeft
+                                ]
+                                iiifLogo
+                            , link
+                                [ linkColour
+                                , alignLeft
+                                ]
+                                { url = body.url
+                                , label = text "(Manifest)"
+                                }
+                            ]
+                        ]
+                    ]
+
+                _ ->
+                    [ link
+                        [ linkColour ]
+                        { url = body.url
+                        , label = text body.url
+                        }
+                    ]
+    in
     wrappedRow
         [ width fill
         , alignTop
@@ -46,12 +94,8 @@ viewExternalResource language body =
                 [ width fill ]
                 [ renderParagraph language body.label ]
             , row
-                [ width fill ]
-                [ link
-                    [ linkColour ]
-                    { url = body.url
-                    , label = text body.url
-                    }
+                [ width fill
                 ]
+                externalResourceLink
             ]
         ]
