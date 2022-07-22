9623f8fc4c5fd259dc4a0b1e83bd452b2f7f5479
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-07-22 10:53:46 +0200

    Review PAE help text

--- src/Page/Keyboard/Views/PaeHelp.elm
+++ src/Page/Keyboard/Views/PaeHelp.elm
@@ -72,32 +72,39 @@ englishPaeHelpText =
         , link
             [ linkColour ]
             { url = "https://www.iaml.info/plaine-easie-code"
-            , label = text "Plaine and Easie Code"
+            , label = text "Plaine and Easie Code. "
             }
-        , text """ at varying levels of complexity. The most basic query
+        , text """The most basic query
         uses upper-case letters, A-G, to represent pitches; for example, """
         , el [ spacing lineSpacing, Font.family [ Font.monospace ], padding 2, Background.color (colourScheme.lightGrey |> convertColorToElementColor) ] (text "EDCDEEE")
-        , text """. Using the "x", "b", and "n" characters, you can indicate sharps, flats, and naturals, respectively.
-        The incipit preview above will show you what this will look like. """
-        , text """The keyboard input will print the corresponding Plaine and Easie code in the input box, so
-        you can also use this interface to enter a search query. """
-        , text """Search queries must be longer than three pitches."""
+        , text """. You can vary the note properties by using different characters immediately preceding it. The "'"
+        (single quote) character will specify a note in the C4 octave, while the "," (comma) will shift the note to the
+        C3 octave. Successive octave shifts up and down are accomplished by adding additional characters; "''" is a note
+          in the C5 octave, and ",," is a note in the C2 octave. The "x", "b", and "n" characters immediately preceding
+          the pitch name indicates sharps, flats, and naturals, respectively. For example, to query for the
+          B-A-C-H figure, you might specify: """
+        , el [ spacing lineSpacing, Font.family [ Font.monospace ], padding 2, Background.color (colourScheme.lightGrey |> convertColorToElementColor) ] (text "'bB'A''C'nB")
+        , text """The preview will show you what your query will look like as you enter the notes. """
+        , text """The piano keyboard will print the corresponding Plaine and Easie code in the input box as you press
+        the keys, so you can use this input method to enter a search query. Search queries must be longer than three
+        pitches."""
+        , text """Durations, measures, and figures beyond pitch and interval
+        are shown in the preview, but are not used in the search. Support for these features is planned for later versions. """
         ]
     , paragraph
         [ width fill, spacing lineSpacing ]
-        [ text """The Clef, Time signature, and Key signature options also control how the search
+        [ text """The Clef, Time signature, and Key signature options control how the search query preview
          is rendered. In the case of the Key signature field, it will also determine the steps between
-         the notes. However, they will not serve as filters to your search query; that is, indicating
-         an "F-4" clef will only control how the incipit is displayed, it will not limit your searches
-         to only incipits encoded with that clef. If you wish to limit your searches to specific clefs,
-         key signatures, and time signatures, use the facet controls below.""" ]
+         the notes, since it "pre-sets" the pitches that should be raised or lowered from their naturals.
+         However, they will not serve as filters to your search query; that is, indicating an "F-4" clef will only
+         control how the incipit query is displayed, and will not limit your searches to incipits encoded with that
+         clef. If you wish to limit your searches to specific clefs, key signatures, and time signatures, use the
+         controls below.""" ]
     , paragraph
         [ width fill, spacing lineSpacing ]
-        [ text """The Query mode controls how your query in interpreted. If "Intervals" is chosen (the default),
-         then the query will be interpreted as a sequence of intervals, allowing your query to match any
-         transposition. If you are only interested matching exactly the pitches in the query, choose the
-         "Exact pitches" option. This will only match the query in the exact transposition given. """
-        , text """Note durations, measures, octaves, and more extensive notational figures beyond pitch and interval
-        are not used in the search. These are features that are planned for later versions. """
+        [ text """The Query mode option how your query in interpreted. If "Intervals" is chosen (the default),
+         then the query will be interpreted as a sequence of chromatic intervals, and your query will match in any
+         transposition. If you wish to match the pitches exactly, choose the "Exact pitches" option. This will match 
+         the query in the exact transposition given. """
         ]
     ]
