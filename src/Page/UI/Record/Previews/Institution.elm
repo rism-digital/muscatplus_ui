57e7c6de295d2335fbbcce1d2c5ba7d562f49c7d
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-08-18 09:43:21 +0200

    Fixed: Institution previews more comprehensive

--- src/Page/UI/Record/Previews/Institution.elm
+++ src/Page/UI/Record/Previews/Institution.elm
@@ -2,10 +2,12 @@ module Page.UI.Record.Previews.Institution exposing (viewInstitutionPreview)
 
 import Element exposing (Element, alignTop, column, fill, height, paddingXY, row, scrollbarY, spacing, width)
 import Language exposing (Language)
+import Page.Record.Views.InstitutionPage.LocationSection exposing (viewLocationAddressSection)
 import Page.RecordTypes.Institution exposing (InstitutionBody)
-import Page.UI.Attributes exposing (lineSpacing, sectionSpacing)
+import Page.UI.Attributes exposing (lineSpacing, sectionBorderStyles, sectionSpacing)
 import Page.UI.Components exposing (viewSummaryField)
 import Page.UI.Helpers exposing (viewMaybe)
+import Page.UI.Record.ExternalAuthorities exposing (viewExternalAuthoritiesSection)
 import Page.UI.Record.ExternalResources exposing (viewExternalResourcesSection)
 import Page.UI.Record.Notes exposing (viewNotesSection)
 import Page.UI.Record.PageTemplate exposing (pageHeaderTemplate, pageUriTemplate)
@@ -14,6 +16,22 @@ import Page.UI.Record.Relationship exposing (viewRelationshipsSection)
 
 viewInstitutionPreview : Language -> InstitutionBody -> Element msg
 viewInstitutionPreview language body =
+    let
+        summaryBody labels =
+            row
+                (width fill
+                    :: height fill
+                    :: alignTop
+                    :: sectionBorderStyles
+                )
+                [ column
+                    [ width fill
+                    , height fill
+                    , spacing lineSpacing
+                    ]
+                    [ viewSummaryField language labels ]
+                ]
+    in
     row
         [ width fill
         , height fill
@@ -50,10 +68,12 @@ viewInstitutionPreview language body =
                     [ width fill
                     , spacing sectionSpacing
                     ]
-                    [ viewMaybe (viewSummaryField language) body.summary
+                    [ viewMaybe summaryBody body.summary
+                    , viewMaybe (viewLocationAddressSection language) body.location
                     , viewMaybe (viewRelationshipsSection language) body.relationships
                     , viewMaybe (viewNotesSection language) body.notes
                     , viewMaybe (viewExternalResourcesSection language) body.externalResources
+                    , viewMaybe (viewExternalAuthoritiesSection language) body.externalAuthorities
                     ]
                 ]
             ]
