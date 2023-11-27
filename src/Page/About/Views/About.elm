74efb7feb2a89a6a2e9d4c1b628b5d69b99dac3f
Andrew Hankinson <andrew.hankinson@gmail.com>
2023-11-27 14:44:02 +0100

    Update help translations

--- src/Page/About/Views/About.elm
+++ src/Page/About/Views/About.elm
@@ -7,6 +7,7 @@ import Element.Font as Font
 import Language exposing (Language(..), LanguageMap, LanguageValue(..))
 import Page.About.Model exposing (AboutPageModel)
 import Page.About.Msg exposing (AboutMsg(..))
+import Page.About.Views.AboutTexts exposing (aboutTextEnglish, aboutTextGerman, aboutTextItalian)
 import Page.UI.Attributes exposing (headingXL, lineSpacing, sectionSpacing)
 import Page.UI.Markdown as Markdown
 import Page.UI.Style exposing (colourScheme)
@@ -15,87 +16,17 @@ import Session exposing (Session)
 import Time exposing (Month(..), Posix)
 
 
-aboutTextEnglish : String
-aboutTextEnglish =
-    """# About RISM Online
-
-The Répertoire International des Sources Musicales (RISM) - International Inventory
-of Musical Sources - is an international, non-profit organization that aims to comprehensively
-document extant musical sources worldwide: manuscripts, printed music editions, writings on
-music theory, and libretti that are found in libraries, archives, churches, schools, and
-private collections.
-
-RISM Online, a service from [the RISM Digital Center](https://rism.digital)
-provides access to these musical sources through extensive search and browse
-functions. Multiple entry points, with extensive Source, Person, and Institution search
-interfaces, provide users with the ability to quickly narrow down relevant results using several
-different approaches, including keyword searches, filters, and relationship information.
-
-The Incipit query interface provides fast and accurate notation searches,
-with coloured notes that highlight the parts of the incipit that matches a query. Users
-can also narrow down incipits that match by specifying a clef, key signature, or time signature
-as a filter, or combine a notation search with a keyword search for composer or piece names.
-
-The National Collection search filter lets users narrow down all search functionality
-to the bibliography of a single country. This automatically narrows down the search capabilities
-to just the sources, institutions, and musical incipits available in that country. (Since people
-can span multiple countries, they are excluded from the search functions when a National
-Collection is active.)
-
-URLs in RISM Online are guaranteed to be authoritative, stable, and cite-able. You
-can bookmark, cite, and share URLs among colleagues, with an institutional commitment that they
-will persist. This includes links to searches and previews, allowing users to share their complex
-queries with the knowledge that the recipient will see, as close as possible, the same view.
-
-## Open Data
-
-All data in RISM Online is available under a Creative Commons CC-BY 3.0 license.
-You are free to download and modify the data. If you find the data useful, please include a
-link back to RISM Online. We are also interested in knowing how RISM data
-is being used, so please get in touch to tell us about your project.
-
-## Contact
-
-We are constantly improving RISM Online. Feedback about your experience is welcome!
-Let us know if you could not find what you were looking for, or if there is some information
-incorrect or missing.
-
-General feedback can be sent to [feedback@rism.online](mailto:feedback@rism.online).
-
-All records also feature a "Report an issue" link at the bottom of the page, which
-takes you to a form where you can describe the problem. Whenever possible it is helpful to
-include a link to the record or search that triggered the problem so that we can reproduce it.
-The URL of the current page is automatically added to the feedback form submission.
-
-## Credits and licenses
-
-### Background images
-
-**Sources**: Luzzaschi, Luzzasco. 1601. __Madrigali di Luzzasco Luzzaschi per cantare, et sonare a vno, e doi, e tre soprani.__ Rome: Simone Verovio. p. 5.
-[Library of Congress, Music Division, Washington, DC.](https://www.loc.gov/resource/ihas.200154752.0/?sp=15&st=image)
-
-**People**: Coques, Gonzales. ca. 1653. __Jacques van Eyck and His Family (?),__ Museum of Fine Arts, Budapest.
-[Wikimedia Commons.](https://commons.wikimedia.org/wiki/File:Gonzales_Coques_-_Portrait_of_the_Duarte_family.jpg)
-
-**Institutions**: Iliff, David. 2015. The interior of Duke Humphrey's Library, the oldest reading room of the Bodleian Library in the University of Oxford. License: CC BY-SA 3.0.
-[Wikimedia Commons.](https://en.wikipedia.org/wiki/File:Duke_Humfrey%27s_Library_Interior_5,_Bodleian_Library,_Oxford,_UK_-_Diliff.jpg)
-
-**Incipits**: Sarasin, Lukas. [1760–1802]. __Katalog der Lucas Sarasin'schen Musiksammlung.__ Universitätsbibliothek Basel, HKun d III 9. Public Domain.
-[e-Manuscripta.](https://doi.org/10.7891/e-manuscripta-13836)
-"""
-
-
 aboutText : LanguageMap
 aboutText =
-    [ LanguageValue English [ aboutTextEnglish ] ]
+    [ LanguageValue English [ aboutTextEnglish ]
+    , LanguageValue German [ aboutTextGerman ]
+    , LanguageValue Italian [ aboutTextItalian ]
+    ]
 
 
 view : Session -> AboutPageModel -> Element AboutMsg
 view session model =
     let
-        isActive =
-            model.linksEnabled
-
         indexedTimestamp =
             case model.response of
                 Response (AboutData body) ->
--- src/Page/About/Views/Help.elm
+++ src/Page/About/Views/Help.elm
@@ -23,6 +23,8 @@ iconTranslations : LanguageMap
 iconTranslations =
     [ LanguageValue English [ "Icon" ]
     , LanguageValue French [ "Icône" ]
+    , LanguageValue German [ "Symbol" ]
+    , LanguageValue Italian [ "Icona" ]
     ]
 
 
@@ -30,6 +32,8 @@ descriptionTranslations : LanguageMap
 descriptionTranslations =
     [ LanguageValue English [ "Description" ]
     , LanguageValue French [ "Description" ]
+    , LanguageValue German [ "Beschreibung" ]
+    , LanguageValue Italian [ "Descrizione" ]
     ]
 
 
@@ -46,6 +50,14 @@ recordTypesEntries =
             notice bibliographique d'un imprimé. Le contenu de la collection de manuscrits ou de l'imprimé est 
             détaillé avec une liste de notices filles qui peuvent être recherchées à partir de la notice source. (Le 
             contenu de quelques collections de manuscrits n'a cependant pas encore été catalogué).""" ]
+            , LanguageValue German [ """Manuskriptsammlung, die verschiedene Einzelwerke enthält oder bibliografischer
+            Datensatz eines Drucks. Der Inhalt der Handschriftensammlung oder des Drucks wird als Liste von
+            untergeordneten Datensätzen aufgeführt, die vom Sammeldatensatz aus durchsucht werden können. Bei einigen
+            wenigen Sammlungen ist der Inhalt jedoch noch nicht katalogisiert.""" ]
+            , LanguageValue Italian [ """Una raccolta contenente diverse composizioni manoscritte, oppure una scheda 
+            bibliografica di una edizione musicale a stampa. Il contenuto della raccolta manoscritta o della stampa è 
+            dettagliato con un elenco di schede figlie che può essere consultato all’interno della scheda di origine. 
+            (Si noti che di alcune raccolte manoscritte non è ancora stato catalogato il contenuto.)""" ]
             ]
       }
     , { icon = bookOpenCoverSvg
@@ -55,6 +67,10 @@ recordTypesEntries =
             with no detailed list of child records.""" ]
             , LanguageValue French [ """Manuscrit musical indépendant, ou notice bibliographique d'un imprimé sans 
             liste détaillée de notices filles.""" ]
+            , LanguageValue German [ """Individualeintrag einer Musikhandschrift oder des bibliographischen Datensatzes 
+            eines Drucks ohne detaillierte Liste von Untereinträgen.""" ]
+            , LanguageValue Italian [ """Un singolo manoscritto musicale, o una scheda bibliografica di una edizione 
+            musicale a stampa priva di un elenco dettagliato di schede figlie.""" ]
             ]
       }
     , { icon = bookOpenSvg
@@ -66,6 +82,11 @@ recordTypesEntries =
             , LanguageValue French [ """Un élément contenu dans une collection de manuscrits ou dans un imprimé. La 
             notice appartient toujours à un parent, soit une collection de manuscrits, soit une notice 
             bibliographique d'un imprimé.""" ]
+            , LanguageValue German [ """Teileintrag einer Manuskriptsammlung oder eines Drucks. Der Datensatz ist stets 
+            mit einem übergeordneten Sammeleintrag (Manuskript oder Druck) verknüpft.""" ]
+            , LanguageValue Italian [ """Un elemento di contenuto all'interno di una raccolta manoscritta o
+            all'interno di una edizione musicale a stampa. Una tale scheda appartiene sempre a una scheda madre,
+            ovvero a una raccolta manoscritta o a una scheda bibliografica di una edizione musicale a stampa.""" ]
             ]
       }
     , { icon = bookCopySvg
@@ -75,6 +96,10 @@ recordTypesEntries =
             or print items, or collections, have been bound together.""" ]
             , LanguageValue French [ """Un volume composite décrivant un regroupement dans une archive de documents 
             manuscrits ou imprimés, ou de collections, reliés ensemble.""" ]
+            , LanguageValue German [ """Konvolut, in dem mehrere Manuskripte, Drucke oder Sammlungen zusammengebunden 
+            sind. """ ]
+            , LanguageValue Italian [ """Un volume composito che descrive un aggregato archivistico in cui diverse 
+            unità bibliografiche manoscritte o stampate, e/o diverse raccolte, sono state rilegate insieme.""" ]
             ]
       }
     ]
@@ -87,6 +112,8 @@ sourceTypesEntries =
       , description =
             [ LanguageValue English [ "A handwritten manuscript source held in one single library." ]
             , LanguageValue French [ "Source manuscrite conservée dans une seule bibliothèque." ]
+            , LanguageValue German [ "Handschriftliche Quelle, die in einer einzigen Bibliothek aufbewahrt wird." ]
+            , LanguageValue Italian [ "Una fonte manoscritta conservata in una singola biblioteca." ]
             ]
       }
     , { icon = printingPressSvg
@@ -94,6 +121,8 @@ sourceTypesEntries =
       , description =
             [ LanguageValue English [ "A printed source with holdings in one or more libraries." ]
             , LanguageValue French [ "Source imprimée conservée dans une ou plusieurs bibliothèques." ]
+            , LanguageValue German [ "Gedruckte Quelle, die in einer oder mehreren Bibliotheken aufbewahrt wird." ]
+            , LanguageValue Italian [ "Una fonte stampata in possesso di una o più biblioteche." ]
             ]
       }
     , { icon = rectanglesMixedSvg
@@ -102,6 +131,10 @@ sourceTypesEntries =
             [ LanguageValue English [ "A source with mixed types (e.g., a composite volume) held in one single library." ]
             , LanguageValue French [ """Une source avec des types mixtes (par exemple, un volume composite) conservée 
             dans une seule bibliothèque.""" ]
+            , LanguageValue German [ """Quelle mit gemischten Materialien (z. B. ein Konvolut), die in einer einzigen 
+            Bibliothek aufbewahrt wird.""" ]
+            , LanguageValue Italian [ """Una fonte di tipo misto (ad esempio, un volume composito) conservata in una 
+            singola biblioteca.""" ]
             ]
       }
     ]
@@ -115,6 +148,8 @@ contentTypesEntries =
             [ LanguageValue English [ "The content is music notation (e.g., staff notation, tablature notation)." ]
             , LanguageValue French [ """Le contenu est de la notation musicale (par exemple, de la notation en portées, 
             de la notation en tablature).""" ]
+            , LanguageValue German [ "Der Inhalt besteht aus Musiknoten, z. B. übliches Notensystem, Tabulatur etc." ]
+            , LanguageValue Italian [ "La fonte contiene notazione musicale (ad esempio, notazione su pentagramma o intavolatura)." ]
             ]
       }
     , { icon = commentsSvg
@@ -122,6 +157,8 @@ contentTypesEntries =
       , description =
             [ LanguageValue English [ "The content is a libretto of music work, such as an opera" ]
             , LanguageValue French [ "Le contenu est un livret d’une œuvre musicale, tel qu'un opéra" ]
+            , LanguageValue German [ "Der Inhalt besteht aus dem Libretto zu einem musikdramatischen Werk, z. B. zu einer Oper." ]
+            , LanguageValue Italian [ "La fonte contiene il libretto di una composizione musicale, ad esempio un melodramma." ]
             ]
       }
     , { icon = graduationCapSvg
@@ -129,6 +166,8 @@ contentTypesEntries =
       , description =
             [ LanguageValue English [ "The content is a treatise that is a theoretical text, with or without music notation examples." ]
             , LanguageValue French [ "Le contenu est un traité qui est un texte théorique, avec ou sans exemples en notation musicale." ]
+            , LanguageValue German [ "Der Inhalt besteht aus einer musiktheoretischen Abhandlung, mit oder ohne Notationsbeispiele." ]
+            , LanguageValue Italian [ "La fonte contiene il testo di un trattato di teoria, con o senza esempi di notazione musicale." ]
             ]
       }
     , { icon = shapesSvg
@@ -136,6 +175,8 @@ contentTypesEntries =
       , description =
             [ LanguageValue English [ "The content is mixed and cannot be identified with one category above." ]
             , LanguageValue French [ "Le contenu est mixte et ne peut être identifié à l'une des catégories ci-dessus." ]
+            , LanguageValue German [ "Der Inhalt ist gemischt und kann nicht einer der oben genannten Kategorien zugeordnet werden." ]
+            , LanguageValue Italian [ "Il contenuto è misto e non può essere identificato con una delle categorie sopra elencate." ]
             ]
       }
     , { icon = ellipsesSvg
@@ -143,6 +184,8 @@ contentTypesEntries =
       , description =
             [ LanguageValue English [ "The content is of another type (e.g., a map or a drawing)." ]
             , LanguageValue French [ "Le contenu est d'un autre type (par exemple, une carte ou un dessin)." ]
+            , LanguageValue German [ "Der Inhalt ist von anderer Art (z. B. eine Karte oder eine Zeichnung)." ]
+            , LanguageValue Italian [ "Il contenuto è di altro tipo (ad esempio, una mappa o un disegno)." ]
             ]
       }
     ]
@@ -155,6 +198,8 @@ additionalIconsEntries =
       , description =
             [ LanguageValue English [ "The record includes one or more musical incipits." ]
             , LanguageValue French [ "La notice comprend un ou plusieurs incipits musicaux." ]
+            , LanguageValue German [ "Der Datensatz enthält ein oder mehrere Musikincipits." ]
+            , LanguageValue Italian [ "La scheda comprende uno o più incipit musicali." ]
             ]
       }
     , { icon = digitizedImagesSvg
@@ -164,6 +209,12 @@ additionalIconsEntries =
             the link is attached to a specific exemplar, and more than one link can be available.""" ]
             , LanguageValue French [ """La notice comprend un lien vers une reproduction numérique de la source. Pour 
             les imprimés, le lien est attaché à un examplaire spécifique, et plus d'un lien peut être disponible.""" ]
+            , LanguageValue German [ """Der Datensatz enthält einen Link zu einer digitalen Reproduktion der Quelle. 
+            Bei Drucken ist der Link an ein spezifisches Exemplar angehängt. Entsprechend können mehrere Links zu 
+            Digitalisaten vorhanden sein.""" ]
+            , LanguageValue Italian [ """La scheda comprende un collegamento a una riproduzione digitale della fonte. 
+            Per le edizioni musicali a stampa, il collegamento si riferisce a un singolo esemplare, e possono essere 
+            disponibili più collegamenti a riproduzioni di differenti esemplari della medesima edizione.""" ]
             ]
       }
     , { icon = \_ -> iiifLogo
@@ -173,6 +224,8 @@ additionalIconsEntries =
             RISM Online.""" ]
             , LanguageValue French [ """La notice comprend un manifeste IIIF. Les images seront affichées directement 
             dans RISM Online.""" ]
+            , LanguageValue German [ """Der Datensatz enthält einen IIIF-Link. Die Bilder werden direkt in RISM Online angezeigt.""" ]
+            , LanguageValue Italian [ """La scheda comprende un manifesto IIIF. Le immagini verranno visualizzate direttamente in RISM Online.""" ]
             ]
       }
     , { icon = linkSvg
@@ -182,6 +235,10 @@ additionalIconsEntries =
             database also included in RISM Online (e.g., DIAMM).""" ]
             , LanguageValue French [ """La notice comprend un lien vers une version correspondante de la notice dans 
             une autre base de données également incluse dans RISM Online (par exemple, DIAMM).""" ]
+            , LanguageValue German [ """Der Datensatz enthält einen Link zu einer weiteren Version des Datensatzes in 
+            einer anderen Datenbank, die ebenfalls in RISM Online enthalten ist (z. B. DIAMM).""" ]
+            , LanguageValue Italian [ """Der Datensatz enthält einen Link zu einer abgebildeten Version des Datensatzes
+             in einer anderen Datenbank, die ebenfalls in RISM Online enthalten ist (z. B. DIAMM).""" ]
             ]
       }
     ]
@@ -252,6 +309,27 @@ collection ou d'un volume composite. RISM Online affiche différentes icônes po
 notices dans la liste des résultats de la recherche."""
 
 
+helpTextRecordTypesGerman : String
+helpTextRecordTypesGerman =
+    """## Datensatztypen
+
+In der RISM-Datenbank gibt es verschiedene Datensatztypen, die je nach vorhandener Quelle angeben, ob es sich bei
+einem Datensatz um die Beschreibung einer Sammlung (d. h. mit untergeordneten Datensätzen), einen einzelnen Eintrag,
+den Inhalt einer Sammlung oder ein Konvolut handelt. RISM Online zeigt verschiedene Symbole zur Unterscheidung dieser
+Eintragstypen in der Ergebnisliste an.
+"""
+
+
+helpTextRecordTypesItalian : String
+helpTextRecordTypesItalian =
+    """## Tipi di schede
+
+Il database RISM ha diversi tipi di schede per indicare se si tratta della descrizione di una raccolta (ovvero,
+con schede figlie), di un singolo elemento, del contenuto di una raccolta o di un volume composito. RISM Online
+mostra icone diverse per differenziare questi tipi di schede nell'elenco dei risultati di una ricerca.
+"""
+
+
 helpTextSourceTypesEnglish : String
 helpTextSourceTypesEnglish =
     """## Source types
@@ -264,6 +342,20 @@ helpTextSourceTypesFrench =
     """## Types de source
        
 Les types de sources décrivent les caractéristiques matérielles d'une source."""
+
+
+helpTextSourceTypesGerman : String
+helpTextSourceTypesGerman =
+    """## Quellentypen
+
+Quellentypen beschreiben die materiellen Eigenschaften einer Quelle."""
+
+
+helpTextSourceTypesItalian : String
+helpTextSourceTypesItalian =
+    """## Tipi di fonte
+
+I tipi di fonte descrivono le caratteristiche materiali di una fonte."""
 
 
 helpTextContentTypesEnglish : String
@@ -280,6 +372,20 @@ helpTextContentTypesFrench =
 Le type de contenu décrit la forme du matériel contenu dans une source. Une source peut avoir plusieurs types de contenu."""
 
 
+helpTextContentTypesGerman : String
+helpTextContentTypesGerman =
+    """## Inhaltstypen
+
+Der Inhaltstyp beschreibt die Form des Materials innerhalb einer Quelle. Eine Quelle kann mehrere Inhaltstypen aufweisen."""
+
+
+helpTextContentTypesItalian : String
+helpTextContentTypesItalian =
+    """## Tipi di contenuto
+
+Il tipo di contenuto descrive la forma del materiale all'interno di una fonte. Una fonte può riunire diversi tipi di contenuto."""
+
+
 helpTextAdditionalIconsEnglish : String
 helpTextAdditionalIconsEnglish =
     """## Additional icons
@@ -294,6 +400,20 @@ helpTextAdditionalIconsFrench =
 Certaines icônes supplémentaires sont utilisées dans RISM Online pour indiquer que les notices ont des caractéristiques spécifiques."""
 
 
+helpTextAdditionalIconsGerman : String
+helpTextAdditionalIconsGerman =
+    """## Zusätzliche Symbole
+
+In RISM Online werden einige zusätzliche Symbole verwendet, um spezifische Merkmale innerhalb der Datensätze anzuzeigen."""
+
+
+helpTextAdditionalIconsItalian : String
+helpTextAdditionalIconsItalian =
+    """## Icone supplementari
+
+RISM Online utilizza delle icone per indicare alcune caratteristiche specifiche delle singole schede."""
+
+
 helpTextSourceDatabasesEnglish : String
 helpTextSourceDatabasesEnglish =
     """For the databases also included in RISM Online, the records that have no corresponding RISM record will display
@@ -306,10 +426,24 @@ helpTextSourceDatabasesFrench =
 correspondante afficheront une icône indiquant la source de la notice. """
 
 
+helpTextSourceDatabasesGerman : String
+helpTextSourceDatabasesGerman =
+    """Bei den ebenfalls in RISM Online enthaltenen Datenbanken wird für die Datensätze, die keinen entsprechenden
+RISM-Eintrag haben, ein Symbol angezeigt, das den Quelldatensatz angibt."""
+
+
+helpTextSourceDatabasesItalian : String
+helpTextSourceDatabasesItalian =
+    """Le schede delle banche dati esterne incluse in RISM Online che non hanno una scheda RISM corrispondente
+riportano un'icona che ne indica l’origine."""
+
+
 helpTextRecordTypes : LanguageMap
 helpTextRecordTypes =
     [ LanguageValue English [ helpTextRecordTypesEnglish ]
     , LanguageValue French [ helpTextRecordTypesFrench ]
+    , LanguageValue German [ helpTextRecordTypesGerman ]
+    , LanguageValue Italian [ helpTextRecordTypesItalian ]
     ]
 
 
@@ -317,6 +451,8 @@ helpTextSourceTypes : LanguageMap
 helpTextSourceTypes =
     [ LanguageValue English [ helpTextSourceTypesEnglish ]
     , LanguageValue French [ helpTextSourceTypesFrench ]
+    , LanguageValue German [ helpTextSourceTypesGerman ]
+    , LanguageValue Italian [ helpTextSourceTypesItalian ]
     ]
 
 
@@ -324,6 +460,8 @@ helpTextContentTypes : LanguageMap
 helpTextContentTypes =
     [ LanguageValue English [ helpTextContentTypesEnglish ]
     , LanguageValue French [ helpTextContentTypesFrench ]
+    , LanguageValue German [ helpTextContentTypesGerman ]
+    , LanguageValue Italian [ helpTextContentTypesItalian ]
     ]
 
 
@@ -331,6 +469,8 @@ helpTextAdditionalIcons : LanguageMap
 helpTextAdditionalIcons =
     [ LanguageValue English [ helpTextAdditionalIconsEnglish ]
     , LanguageValue French [ helpTextAdditionalIconsFrench ]
+    , LanguageValue German [ helpTextAdditionalIconsGerman ]
+    , LanguageValue Italian [ helpTextAdditionalIconsItalian ]
     ]
 
 
@@ -338,6 +478,8 @@ helpTextSourceDatabases : LanguageMap
 helpTextSourceDatabases =
     [ LanguageValue English [ helpTextSourceDatabasesEnglish ]
     , LanguageValue French [ helpTextSourceDatabasesFrench ]
+    , LanguageValue German [ helpTextSourceDatabasesGerman ]
+    , LanguageValue Italian [ helpTextSourceDatabasesItalian ]
     ]
 
 
@@ -446,8 +588,7 @@ view session =
                         ]
                     , data =
                         [ { description =
-                                [ LanguageValue English [ "DIAMM (Digital Image Archive of Medieval Music)" ]
-                                , LanguageValue French [ "DIAMM (Digital Image Archive of Medieval Music)" ]
+                                [ LanguageValue None [ "DIAMM (Digital Image Archive of Medieval Music)" ]
                                 ]
                           , icon = diammLogo
                           }
