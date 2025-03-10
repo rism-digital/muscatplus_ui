0dfa09f87c161db452c0ae6276d5f308ada9c04c
Andrew Hankinson <andrew.hankinson@gmail.com>
2025-03-10 09:52:01 +0100

    Switch to swc for minifying

--- optimize.sh
+++ optimize.sh
@@ -6,8 +6,19 @@ js=$2
 min=$3
 
 elm-optimize-level-2 --optimize-speed --output=$js $1
-uglifyjs $js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,unsafe_comps,unsafe,drop_console' --mangle 'reserved=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9]' --output $min
+INITIAL_SIZE=$(wc -c < "$js")
 
-echo "Initial size: $(cat $js | wc -c) bytes  ($js)"
-echo "Minified size:$(cat $min | wc -c) bytes  ($min)"
-echo "Gzipped size: $(cat $min | gzip -c | wc -c) bytes"
+swc "$js" --out-file "$min"
+
+MINIFIED_SIZE=$(wc -c < "$min")
+GZIPPED_SIZE=$(gzip -c "$min" | wc -c)
+
+# Convert to human-readable format
+INITIAL_HR=$(numfmt --to=iec-i --suffix=B "$INITIAL_SIZE")
+MINIFIED_HR=$(numfmt --to=iec-i --suffix=B "$MINIFIED_SIZE")
+GZIPPED_HR=$(numfmt --to=iec-i --suffix=B "$GZIPPED_SIZE")
+
+# Display results with alignment
+printf "%-18s %10s (%7s)  %s\n" "Initial size:" "$INITIAL_SIZE bytes" "${INITIAL_HR}" "$js"
+printf "%-18s %10s (%7s)  %s\n" "Minified size:" "$MINIFIED_SIZE bytes" "${MINIFIED_HR}" "$min"
+printf "%-18s %10s (%7s)\n" "Gzipped size:" "$GZIPPED_SIZE bytes" "${GZIPPED_HR}"
